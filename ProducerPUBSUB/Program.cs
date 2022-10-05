9b09c0effeeeedae37e9d159b1db6e373d4f6e40
magals <itengineer@live.ru>
2022-10-05 20:39:11 +0300

    add: RabbitQM project for pub/sub

---
+++ ProducerPUBSUB/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory { HostName = "localhost" };
+			using var connection = factory.CreateConnection();
+			using var channel = connection.CreateModel();
+
+			channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+			var message = $"Hello i want to broadcast this message";
+			var encoderMessage = Encoding.UTF8.GetBytes(message);
+			channel.BasicPublish("pubsub", "letterbox", null, encoderMessage);
+			Console.WriteLine($"Send message:{message}");
+
+		}
+	}
+}
---
+++ SecondConsumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory { HostName = "localhost" };
+			using var connection = factory.CreateConnection();
+			using var channel = connection.CreateModel();
+
+			channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+			var queuename = channel.QueueDeclare().QueueName;
+
+			
+			var consumer = new EventingBasicConsumer(channel);
+
+			channel.QueueBind(queuename, "pubsub", "");
+
+			consumer.Received += (model, ea) =>
+			{
+				var body = ea.Body.ToArray();
+				var message = Encoding.UTF8.GetString(body);
+				Console.WriteLine($"Seconf consumer - Recieved new message: {message}");
+
+			};
+
+			channel.BasicConsume(queue: queuename, autoAck: true, consumer: consumer);
+			Console.WriteLine($"Consuming");
+			Console.ReadKey();
+
+		}
+	}
+}
