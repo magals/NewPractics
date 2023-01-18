716b530c674842aec36072a5292e517cee0e9529
magals <itengineer@live.ru>
2023-01-19 00:52:37 +0300

    add: ConsoleSG and LibrarySG learn sourcegenerator

---
+++ ConsoleSG/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ConsoleSG
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] pathdrivers = Directory.GetFiles("C:\\Users\\ITEng\\source\\repos\\NewPractics\\LibrarySC\\bin\\Debug\\netstandard2.0", "*.dll", SearchOption.AllDirectories);
+			List<FileInfo> fileInfos = new();
+			foreach (var file in pathdrivers)
+			{
+				FileInfo info = new(file);
+				fileInfos.Add(info);
+			}
+
+			foreach (var item in fileInfos)
+			{
+				var context = new AssemblyLoadContext(name: item.Name, isCollectible: true);
+				try
+				{
+					Assembly assembly = context.LoadFromAssemblyPath(item.FullName);
+					// if true need unload
+					bool FlagUnload = true;
+
+
+				}
+				catch (Exception ex)
+				{
+					context.Unload();
+				}
+			}
+
+			HelloFrom("Generated Code");
+			Console.Read();
+		}
+
+		static partial void HelloFrom(string name);
+	}
+}
