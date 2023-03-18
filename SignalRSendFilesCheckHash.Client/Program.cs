178a9f70f04f2c50e516f2f02ee669a3b1e6d530
magals <itengineer@live.ru>
2023-03-18 21:47:15 +0300

    add: serilog to client

--- SignalRSendFilesCheckHash.Client/Program.cs
+++ SignalRSendFilesCheckHash.Client/Program.cs
@@ -1,10 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 using SignalRSendFilesCheckHash.Models;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+    .Enrich.WithMachineName()
+    .Enrich.WithThreadId()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithEnvironmentUserName()
+    .Enrich.WithClientIp()
+    .Enrich.WithClientAgent()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341/", apiKey: "b9HQGJhY8swnzp5b1biv")
+    .CreateLogger();
+
 try
 {
-
+  Log.Logger.Information("Start Program");
   HubConnection connection = new HubConnectionBuilder()
                 .WithUrl("http://localhost:5206" + "/SyncHub")
                 .AddMessagePackProtocol()
@@ -17,7 +33,7 @@ try
   {
       await foreach (var file in connection.StreamAsync<FileDTO>("SyncGetFileListAsync"))
       {
-        Console.WriteLine("file name:{0} file.Content:{1}", file.FileName, file.Content.Length);
+      Log.Logger.Information("file name:{0} file.Content:{1}", file.FileName, file.Content.Length);
         await File.WriteAllBytesAsync("assets//" + file.FileName, file.Content);
       }
       GC.Collect();
@@ -25,7 +41,12 @@ try
   }).Wait();
 
 }
-catch (Exception e)
+catch (Exception ex)
 {
-  Console.WriteLine("Error" + e.Message);
+  Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+  Log.Logger.Information("Close Program");
+  Log.CloseAndFlush();
 }
---
+++ SignalRSendFilesCheckHash.Server/Services/ReadHashContentService.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRSendFilesCheckHash.Server.Services;
+
+public class ReadHashContentService : BackgroundService
+{
+  public ReadHashContentService()
+  {
+    
+  }
+  protected override Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    throw new NotImplementedException();
+  }
+}
