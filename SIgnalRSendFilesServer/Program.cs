6d72a7762e2f4bc2fe15b2201733476ca3b0480d
magals <itengineer@live.ru>
2023-03-18 17:22:56 +0300

    add: SignalRProject for sends files

---
+++ SIgnalRSendFilesServer/Program.cs
@@ -0,0 +1,7 @@
+using SIgnalRSendFilesServer;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+app.MapHub<SyncHub>("/SyncHub");
+app.Run();
---
+++ SIgnalRSendFilesServer/SyncHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSendFilesModels;
+using System.Text;
+
+namespace SIgnalRSendFilesServer;
+
+public class SyncHub : Hub
+{
+  FileInfo[] Files;
+  public SyncHub()
+  {
+    DirectoryInfo directinfo = new DirectoryInfo(@"assets");
+    Files = directinfo.GetFiles();
+  }
+  public async IAsyncEnumerable<FileDTO> SyncGetFileListAsync()
+  {
+    
+    foreach (var file in Files)
+    {
+      await Task.Delay(1);
+      yield return new FileDTO
+      {
+        FileName = file.Name,
+        Extension = file.Extension,
+        Content = File.ReadAllBytes(file.FullName)
+      };
+    }
+    GC.Collect();
+  }
+
+  public override Task OnConnectedAsync()
+  {
+    Console.WriteLine("OnConnectedAsync");
+    return base.OnConnectedAsync();
+  }
+}
