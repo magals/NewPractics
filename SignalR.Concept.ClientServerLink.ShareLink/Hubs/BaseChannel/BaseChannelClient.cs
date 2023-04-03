531e8604adac96713e534032df1128f2a6ea26de
magals <itengineer@live.ru>
2023-04-03 22:18:31 +0300

    add: project SignalR.Concept.ClientServerLink.ShareLink.csproj

---
+++ SignalR.Concept.ClientServerLink.ShareLink/Hubs/BaseChannel/BaseChannelClient.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalR.Concept.ClientServerLink.ShareLink.Hubs.BaseChannel;
+
+public class BaseChannelClient : IBaseChannelClient_Receiver, IHubConnectionObserver, IDisposable
+{
+  private readonly IBaseChannelClient_Transmiser _hub;
+  private readonly IDisposable _subscription;
+  private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+  public BaseChannelClient(HubConnection connection)
+  {
+    _hub = connection.CreateHubProxy<IBaseChannelClient_Transmiser>(_cancellationTokenSource.Token);
+    _subscription = connection.Register<IBaseChannelClient_Receiver>(this);
+  }
+
+  public void Dispose()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task OnClosed(Exception? exception)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task OnReconnected(string? connectionId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task OnReconnecting(Exception? exception)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ SignalR.Concept.ClientServerLink.ShareLink/Hubs/BaseChannel/IBaseChannelClient_Receiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.Concept.ClientServerLink.ShareLink.Hubs.BaseChannel
+{
+  public interface IBaseChannelClient_Receiver
+  {
+  }
+}
