a679910c62917dc2a1eaa4238339d9597a7fa31e
magals <itengineer@live.ru>
2023-07-17 23:07:04 +0300

    add: items for template mudblazor

--- SimpleExampleMadBlazor/Program.cs
+++ SimpleExampleMadBlazor/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MudBlazor.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
